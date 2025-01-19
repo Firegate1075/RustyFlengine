68f00e9bc3bb59b9c56651c162ec91c94fab848d
Firegate1075 <linus.mueller03@gmail.com>
2025-01-19 23:01:16 +0100

    Add datamodel

---
+++ src/main.rs
@@ -0,0 +1,15 @@
+use crate::datamodel::board::Board;
+use crate::datamodel::chess_move::ChessMove;
+use crate::datamodel::enums::rank::Rank;
+use crate::datamodel::enums::file::File;
+use crate::datamodel::field::Field;
+
+mod datamodel;
+
+fn main() {
+    println!("Hello, world!");
+
+    let mut board: Board = Board::new();
+    let move_to_play: ChessMove = ChessMove::new(Field::new(File::E, Rank::TWO), Field::new(File::E, Rank::FOUR), None);
+    board.play_move(&move_to_play);
+}
